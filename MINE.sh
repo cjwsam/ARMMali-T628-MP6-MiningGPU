3c04424a49b799826d0b02f7d637fd6a01dd1328
cjwsam <cjwsam@gmail.com>
2024-07-01 09:50:23 +0200

    Update MINE.sh

--- MINE.sh
+++ MINE.sh
@@ -6,4 +6,4 @@ export GPU_MAX_ALLOC_PERCENT=100
 export GPU_SINGLE_ALLOC_PERCENT=100
 export GPU_MAX_HEAP_SIZE=100
 
-./sgminer -k cryptonight -o stratum+tcp://pool.supportxmr.com:3333 -u 82eKKLyYeuVhjDKfBhX4pi6nAqmzrsFCvA7DE25L4E6j2hRXBvyDeth4eftoQFBox2jAvUeCqwV1QAn3cydKN4o9ESud6tB -p x  -I 7 -w 32 -d 0,1 --thread-concurrency 8192 --monero --pool-no-keepalive
+./sgminer -k cryptonight -o stratum+tcp://pool.supportxmr.com:3333 -u 48QVbfvwkqzQofZTYDmcVEzKLbAqWEQ4iXCPYVshvCf5tW9zAnwvRf8N4uXmjJ91tACtSdf9ocwrJLoRqJxmwoTU4ebJy16 -p x  -I 7 -w 32 -d 0,1 --thread-concurrency 8192 --monero --pool-no-keepalive
